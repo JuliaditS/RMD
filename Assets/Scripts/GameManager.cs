ed494a9dddb0f4cfc43f82c75e493387603dc80b
catx <ivanshadow19@gmail.com>
2022-06-06 22:42:19 +0700

    Migration

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int getStars;
+    public int uang;
+
+
+    #region Singleton
+    private static GameManager _instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<GameManager>();
+            }
+
+            return _instance;
+        }
+    }
+    #endregion
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public int getBintang(int levelNumber)
+    {
+        return PlayerPrefs.GetInt("SaveStars" + levelNumber);
+    }
+
+    public void SaveData(int levelNumber)
+    {
+        PlayerPrefs.SetInt("levelReach", levelNumber + 1);
+        PlayerPrefs.SetInt("SaveUang", uang + PlayerPrefs.GetInt("SaveUang"));
+        PlayerPrefs.SetInt("SaveStars" + levelNumber, getStars);
+        PlayerPrefs.Save();
+    }
+
+    //
+}
---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour
+{
+   
+
+    public Makanan selectedMakanan;
+
+    [Header("Makanan Description")]
+    public MakananDescription makananDescription;
+    public Image gambar;
+    public Makanan makanan;
+    public TextMeshProUGUI deskripsi;
+    public Button btnExit;
+
+
+    public PopupSlider popupSlider;
+    public GameObject popupPanel;
+    public GameObject popupExitBtn;
+    public GameObject panelBlur;
+
+
+    #region Singleton
+    private static MenuManager _instance;
+
+    public static MenuManager Instance
+    {
+        get
+        {
+
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<MenuManager>();
+            }
+
+            return _instance;
+        }
+    }
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (selectedMakanan == null) return;
+        gambar.sprite = selectedMakanan.sprite;
+        deskripsi.text = selectedMakanan.description;
+    }
+
+    
+    public void GoToLevel(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+
+    public void ResetProgress()
+    {
+
+        // PlayerPrefs.SetInt("levelReach", 1);
+        Destroy(MenuSound.Instance.gameObject);
+        PlayerPrefs.DeleteAll();
+        MenuSound.Instance.source.Stop();
+        Destroy(GameManager.Instance.gameObject);
+
+        SceneManager.LoadScene(1);
+    //     Debug.Log(PlayerPrefs.GetInt("levelReach"));
+    //     Debug.Log(PlayerPrefs.GetInt("SaveUang"));
+    //     Debug.Log(PlayerPrefs.GetInt("SaveStars" + 1));
+    }
+
+    public void CloseApp()
+    {
+        Application.Quit();
+    }
+
+
+   
+}
+
+
